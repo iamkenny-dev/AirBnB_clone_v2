fa85bb990c63541f1abf65c0573124f9df4aea7b
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2022-12-07 22:20:18 +0300

    fix trial

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-"""from models.base_model import BaseModel, Base"""
-from models import storage_type
+"""from models.base_model import BaseModel, Base
+from models import storage_type"""
 from sqlalchemy import Column, String
 
 
